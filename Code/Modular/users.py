53ee123946351d343047a633eb1f97ed6f75e97d
Yohan <94430931+takoyakiyam@users.noreply.github.com>
2024-06-28 14:30:09 +0800

    Update users.py

--- Code/Modular/users.py
+++ Code/Modular/users.py
@@ -6,62 +6,244 @@ class UsersTab(QtWidgets.QWidget):
 
     def __init__(self, parent=None):
         super(UsersTab, self).__init__(parent)
-        self.layout = QtWidgets.QVBoxLayout(self)
-
-        self.horizontalLayout = QtWidgets.QHBoxLayout()
-        self.search_input = QtWidgets.QLineEdit(self)
-        self.search_input.setFixedHeight(40)
-        self.search_input.setPlaceholderText("Search users...")
-
-        self.search_button = QtWidgets.QPushButton("Search", self)
-        self.search_button.setFixedHeight(40)
+        self.setupUi(self)
         self.search_button.clicked.connect(self.search_users)
-        self.horizontalLayout.addWidget(self.search_input)
-        self.horizontalLayout.addWidget(self.search_button)
-        self.layout.addLayout(self.horizontalLayout)
+        self.modify_button.clicked.connect(self.open_modify_user_dialog)
+        self.changeStatus_button.clicked.connect(self.void_user)
+        self.tableWidget.itemSelectionChanged.connect(self.on_selection_changed)
+        self.load_data()
 
-        self.tableWidget = QtWidgets.QTableWidget()
+    def setupUi(self, UsersTab):
+        UsersTab.setObjectName("UsersTab")
+        UsersTab.resize(1332, 747)
+        UsersTab.setStyleSheet("background-color:#fff;")
+        self.gridLayout = QtWidgets.QGridLayout(UsersTab)
+        self.gridLayout.setContentsMargins(10, 10, 10, 10)
+        self.gridLayout.setVerticalSpacing(10)
+        self.gridLayout.setObjectName("gridLayout")
+        self.verticalLayout = QtWidgets.QVBoxLayout()
+        self.verticalLayout.setSpacing(15)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_2.setSpacing(3)
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        spacerItem = QtWidgets.QSpacerItem(10, 20, QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_2.addItem(spacerItem)
+        self.search_button = QtWidgets.QPushButton(UsersTab)
+        self.search_button.setMinimumSize(QtCore.QSize(50, 50))
+        self.search_button.setMaximumSize(QtCore.QSize(50, 50))
+        font = QtGui.QFont()
+        font.setFamily("Segoe UI")
+        font.setPointSize(14)
+        font.setBold(False)
+        font.setItalic(False)
+        font.setWeight(50)
+        font.setStrikeOut(False)
+        self.search_button.setFont(font)
+        self.search_button.setMouseTracking(True)
+        self.search_button.setTabletTracking(True)
+        self.search_button.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.search_button.setStyleSheet("QPushButton {\n"
+" background-color: #f6f4f4;\n"
+"border-radius:25px;\n"
+"color:black;\n"
+";\n"
+"}\n"
+"QPushButton#quit_button {\n"
+"   background-color: green;\n"
+"}\n"
+"QPushButton::pressed {\n"
+"background-color: #fff;\n"
+"}\n"
+"QpushButton{\n"
+"border: 2px solid #555;\n"
+"    border-radius: 20px;\n"
+"    border-style: outset;\n"
+"border-width:200px;\n"
+"\n"
+"}\n"
+"QPushButton:hover {\n"
+"   background-color: #81cdc6;\n"
+"   transition: background-color 0.5s cubic-bezier(0.4, 0, 0.2, 1);\n"
+"color:#fff;\n"
+"}\n"
+"\n"
+"shoppingbag:hover{\n"
+"color:#fff;\n"
+"background-repeat:no-repeat;\n"
+"}\n"
+"\n"
+"border:none;\n"
+"")
+        self.search_button.setText("")
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(":/search/search.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.search_button.setIcon(icon)
+        self.search_button.setIconSize(QtCore.QSize(20, 20))
+        self.search_button.setObjectName("search_button")
+        self.horizontalLayout_2.addWidget(self.search_button)
+        self.search_input = QtWidgets.QLineEdit(UsersTab)
+        self.search_input.setMinimumSize(QtCore.QSize(300, 50))
+        self.search_input.setMaximumSize(QtCore.QSize(600, 75))
+        self.search_input.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.search_input.setStyleSheet("border-radius:25px;\n"
+"padding-right:10px;\n"
+"padding-left:10px;\n"
+"background-color:#f6f4f4;\n"
+"")
+        self.search_input.setText("")
+        self.search_input.setCursorPosition(0)
+        self.search_input.setDragEnabled(False)
+        self.search_input.setCursorMoveStyle(QtCore.Qt.LogicalMoveStyle)
+        self.search_input.setObjectName("search_input")
+        self.horizontalLayout_2.addWidget(self.search_input)
+        spacerItem1 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_2.addItem(spacerItem1)
+        self.verticalLayout.addLayout(self.horizontalLayout_2)
+        self.tableWidget = QtWidgets.QTableWidget(UsersTab)
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.tableWidget.setFont(font)
+        self.tableWidget.setStyleSheet("")
+        self.tableWidget.setObjectName("tableWidget")
         self.tableWidget.horizontalHeader().setStretchLastSection(True)
         self.tableWidget.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Stretch)
         self.tableWidget.verticalHeader().setVisible(False)
-
-        # set table colors
-        self.tableWidget.setStyleSheet(
-            "QTableWidget::item:selected { background-color: #3b352d; }"
-            "QHeaderView::section { background-color: #ff7d7d; }"
-        )
-
-        self.layout.addWidget(self.tableWidget)
-
-        self.horizontalLayout_4 = QtWidgets.QHBoxLayout()
-        
-
-        self.modifyUser_button = QtWidgets.QPushButton(self)
+        self.tableWidget.setStyleSheet("QHeaderView::section { background-color: #88ccc4; }")
+        self.tableWidget.setColumnCount(5)
+        self.tableWidget.setRowCount(0)
+        item = QtWidgets.QTableWidgetItem()
+        item.setBackground(QtGui.QColor(0, 0, 0))
+        self.tableWidget.setHorizontalHeaderItem(0, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(1, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(2, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(3, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(4, item)
+        self.tableWidget.horizontalHeader().setDefaultSectionSize(150)
+        self.tableWidget.horizontalHeader().setMinimumSectionSize(100)
+        self.tableWidget.horizontalHeader().setSortIndicatorShown(False)
+        self.tableWidget.horizontalHeader().setStretchLastSection(True)
+        self.verticalLayout.addWidget(self.tableWidget)
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setContentsMargins(300, -1, 300, -1)
+        self.horizontalLayout.setSpacing(20)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        spacerItem2 = QtWidgets.QSpacerItem(150, 20, QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem2)
+        self.modify_button = QtWidgets.QPushButton(UsersTab)
+        self.modify_button.setMinimumSize(QtCore.QSize(175, 50))
+        self.modify_button.setMaximumSize(QtCore.QSize(350, 60))
         font = QtGui.QFont()
-        font.setPointSize(20)
-        self.modifyUser_button.setFont(font)
-        self.modifyUser_button.setObjectName("modifyUser_button")
-        self.modifyUser_button.setFixedHeight(40)  # Adjust height as needed
-        self.modifyUser_button.clicked.connect(self.open_modify_user_dialog)
-        self.modifyUser_button.setIcon(QtGui.QIcon("edit_icon.png"))
-        self.modifyUser_button.setIconSize(QtCore.QSize(36, 36))
-        self.horizontalLayout_4.addWidget(self.modifyUser_button)
-
-        self.voidProduct_button = QtWidgets.QPushButton(self)
+        font.setFamily("Segoe UI")
+        font.setPointSize(8)
+        font.setBold(True)
+        font.setWeight(75)
+        font.setStrikeOut(False)
+        self.modify_button.setFont(font)
+        self.modify_button.setMouseTracking(True)
+        self.modify_button.setTabletTracking(True)
+        self.modify_button.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.modify_button.setStyleSheet("QPushButton {\n"
+" background-color: #10cc94;\n"
+"border-radius:12px;\n"
+"color:#fff;\n"
+"}\n"
+"QPushButton#quit_button {\n"
+"   background-color: green;\n"
+"}\n"
+"QPushButton::pressed {\n"
+"background-color: #fff;\n"
+"}\n"
+"QpushButton{\n"
+"border: 2px solid #555;\n"
+"    border-radius: 20px;\n"
+"    border-style: outset;\n"
+"border-width:200px;\n"
+"    \n"
+"}\n"
+"QPushButton:hover {\n"
+"   background-color: #0a9c73;\n"
+"   transition: background-color 0.5s cubic-bezier(0.4, 0, 0.2, 1);\n"
+"}\n"
+"\n"
+"border:none;\n"
+"")
+        icon1 = QtGui.QIcon()
+        icon1.addPixmap(QtGui.QPixmap(":/new/Shop/Shoppingcart.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.modify_button.setIcon(icon1)
+        self.modify_button.setIconSize(QtCore.QSize(22, 22))
+        self.modify_button.setObjectName("modify_button")
+        self.horizontalLayout.addWidget(self.modify_button)
+        self.changeStatus_button = QtWidgets.QPushButton(UsersTab)
+        self.changeStatus_button.setMinimumSize(QtCore.QSize(175, 50))
+        self.changeStatus_button.setMaximumSize(QtCore.QSize(350, 60))
         font = QtGui.QFont()
-        font.setPointSize(20)
-        self.voidProduct_button.setFont(font)
-        self.voidProduct_button.setObjectName("voidProduct_button")
-        self.voidProduct_button.setFixedHeight(40)  # Adjust height as needed
-        self.voidProduct_button.clicked.connect(self.void_user)
-        self.voidProduct_button.setIcon(QtGui.QIcon("bin_icon.png"))
-        self.voidProduct_button.setIconSize(QtCore.QSize(36, 36))
-        self.horizontalLayout_4.addWidget(self.voidProduct_button)
+        font.setFamily("Segoe UI")
+        font.setPointSize(8)
+        font.setBold(True)
+        font.setWeight(75)
+        font.setStrikeOut(False)
+        self.changeStatus_button.setFont(font)
+        self.changeStatus_button.setMouseTracking(True)
+        self.changeStatus_button.setTabletTracking(True)
+        self.changeStatus_button.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.changeStatus_button.setStyleSheet("QPushButton {\n"
+" background-color: #F88379;\n"
+"border-radius:12px;\n"
+"color:#fff;\n"
+"}\n"
+"QPushButton#quit_button {\n"
+"   background-color: green;\n"
+"}\n"
+"QPushButton::pressed {\n"
+"background-color: #fff;\n"
+"}\n"
+"QpushButton{\n"
+"border: 2px solid #555;\n"
+"    border-radius: 20px;\n"
+"    border-style: outset;\n"
+"border-width:200px;\n"
+"    \n"
+"}\n"
+"QPushButton:hover {\n"
+"   background-color: #E5676B;\n"
+"   transition: background-color 0.5s cubic-bezier(0.4, 0, 0.2, 1);\n"
+"}\n"
+"\n"
+"border:none;\n"
+"")
+        self.changeStatus_button.setIcon(icon1)
+        self.changeStatus_button.setIconSize(QtCore.QSize(22, 22))
+        self.changeStatus_button.setObjectName("changeStatus_button")
+        self.horizontalLayout.addWidget(self.changeStatus_button)
+        spacerItem3 = QtWidgets.QSpacerItem(150, 20, QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem3)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+        self.gridLayout.addLayout(self.verticalLayout, 0, 0, 1, 1)
 
-        self.layout.addLayout(self.horizontalLayout_4)
+        self.retranslateUi(UsersTab)
+        QtCore.QMetaObject.connectSlotsByName(UsersTab)
 
-        self.tableWidget.itemSelectionChanged.connect(self.on_selection_changed)
-        self.load_data()
+    def retranslateUi(self, UsersTab):
+        _translate = QtCore.QCoreApplication.translate
+        UsersTab.setWindowTitle(_translate("UsersTab", "Users"))
+        self.search_input.setPlaceholderText(_translate("UsersTab", "  Search Users..."))
+        item = self.tableWidget.horizontalHeaderItem(0)
+        item.setText(_translate("UsersTab", "First Name"))
+        item = self.tableWidget.horizontalHeaderItem(1)
+        item.setText(_translate("UsersTab", "Last Name"))
+        item = self.tableWidget.horizontalHeaderItem(2)
+        item.setText(_translate("UsersTab", "Username"))
+        item = self.tableWidget.horizontalHeaderItem(3)
+        item.setText(_translate("UsersTab", "Password"))
+        item = self.tableWidget.horizontalHeaderItem(4)
+        item.setText(_translate("UsersTab", "Level of Access"))
+        self.modify_button.setText(_translate("UsersTab", "Modify"))
+        self.changeStatus_button.setText(_translate("UsersTab", "Deactivate"))
 
     def load_data(self, search_query=None):
         conn = sqlite3.connect('j7h.db')
@@ -90,15 +272,9 @@ class UsersTab(QtWidgets.QWidget):
 
         conn.close()
 
-    def on_selection_changed(self):
-        selected_rows = set()
-        for item in self.tableWidget.selectedItems():
-            selected_rows.add(item.row())
-        for row in selected_rows:
-            for column in range(self.tableWidget.columnCount()):
-                item = self.tableWidget.item(row, column)
-                if item:
-                    item.setSelected(True)
+    def search_users(self):
+        search_query = self.search_input.text()
+        self.load_data(search_query)
 
     def open_modify_user_dialog(self):
         selected_row = self.tableWidget.currentRow()
@@ -130,7 +306,7 @@ class UsersTab(QtWidgets.QWidget):
                 reply = QtWidgets.QMessageBox.question(
                     None,
                     'Confirmation',
-                    'Are you sure you want to void this user?',
+                    'Are you sure you want to deactivate this user?',
                     QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No,
                     QtWidgets.QMessageBox.No
                 )
@@ -145,10 +321,15 @@ class UsersTab(QtWidgets.QWidget):
         else:
             QtWidgets.QMessageBox.warning(None, "Selection Error", "Please select a user to void.")
 
-    def search_users(self):
-        search_query = self.search_input.text()
-        self.load_data(search_query)
-
+    def on_selection_changed(self):
+        selected_rows = set()
+        for item in self.tableWidget.selectedItems():
+            selected_rows.add(item.row())
+        for row in selected_rows:
+            for column in range(self.tableWidget.columnCount()):
+                item = self.tableWidget.item(row, column)
+                if item:
+                    item.setSelected(True)
 
 class ModifyUserDialog(QtWidgets.QDialog):
     def __init__(self, rowid, user, parent=None):
@@ -224,12 +405,3 @@ class ModifyUserDialog(QtWidgets.QDialog):
         conn.close()
 
         self.accept()
-
-if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = UsersTab()
-    MainWindow.setCentralWidget(ui)
-    MainWindow.show()
-    sys.exit(app.exec_())
